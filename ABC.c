463e2c05c557b5d671e5cdfc2c0bb90b4a1afb8f
gry-kiu <gry17@kiu.kr>
2021-09-21 19:17:15 +0900

    Update ABC.c

--- ABC.c
+++ ABC.c
@@ -229,7 +229,7 @@ void SendScoutBees() {
   }
 }
 
-void standard_ABC(const int gnty_s, const double *const intl_lwr_bnd, const double *const intl_uppr_bnd, const double *const srch_lwr_bnd, const double *const srch_uppr_bnd, double (*objective_function)(double *const x, const int gnty_s), void (*callback_function)(const double GlobalMin, const double *const GlobalParams), const int evlt_mx, const int arg_NP, const int arg_FoodNumber, const int arg_limit) {
+void standard_ABC(const int gnty_s, const double *const intl_lwr_bnd, const double *const intl_uppr_bnd, const double *const srch_lwr_bnd, const double *const srch_uppr_bnd, double (*objective_function)(double *const solution, const int D), void (*callback_function)(const double GlobalMin, const double *const GlobalParams), const int evlt_mx, const int arg_NP, const int arg_FoodNumber, const int arg_limit) {
   // -------------------- initialization --------------------
 
   int evlt_c = 0;
